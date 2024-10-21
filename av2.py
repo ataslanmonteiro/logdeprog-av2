fe569643c6e9ef0f5800e03fba95981b197671c2
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2024-10-21 16:41:50 -0300

    Finalizado ok

--- av2.py
+++ av2.py
@@ -1,20 +1,21 @@
-# Função para inserir os dados de cada lançamento
+# Função para inserir os dados de um lançamento
 def inserir_lancamento():
-    codigo = input("Digite o código de ID do produto: ")
+    codigo = input("Digite o ID do produto: ")
     quant = int(input("Digite a quantidade do produto: "))
     valor_unit = float(input("Digite o valor unitário do produto: "))
     return codigo, quant, valor_unit
 
-#Inicia a variável que irá guardar o valor total a ser pago
-    valor_total = 0
+# Inicia a variável que irá guardar o valor total a ser pago
+valor_total = 0
 
-#Insere os dados de 5 lançamentos
-    for i in range(5):
-    print(f"Lançamento {i+1}")
+# Captura os dados de 5 lançamentos
+for i in range(5):
+    print(f"Lançamento do produto {i+1}")
     codigo, quant, valor_unit = inserir_lancamento()
     valor_total += quant * valor_unit
-    print(f"Subtotal após lançamento {i+1}: R$ {valor_total:.2f}\n")
+    print(f"Subtotal após lançamento do produto {i+1}: R$ {valor_total:.2f}\n")
 
-#Valor total dos 5 lançamentos a ser pago
-    print(f"Valor total dos 5 lançamentos a ser pago: R$ {valor_total:.2f} ")
+# Mostra o valor total a ser pago após os 5 lançamentos
+print(f"Valor total a ser pago: R$ {valor_total:.2f}")
+
     

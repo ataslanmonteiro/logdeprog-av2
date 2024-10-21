67de579c403dca51b84d0993578803fa1491cabc
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2024-10-21 20:45:39 -0300

    Tentativa 1

---
+++ 2.2.py
@@ -0,0 +1,33 @@
+import math
+
+# Função para calcular as raízes usando a fórmula de Baskara
+def calcular_raizes(a, b, c):
+    if a == 0:
+        return "Impossível calcular: O valor de 'a' não pode ser zero."
+    
+    delta = b ** 2 - 4 * a * c
+
+    if delta < 0:
+        return "Impossível calcular: O valor de 'delta' não pode ser negativo"
+    
+    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+    raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+
+    return raiz1, raiz2
+
+# Captura os coeficientes da equação
+    a = float(input("Digite o valor de 'a': "))
+    b = float(input("Digite o valor de 'b': "))
+    c = float(input("Digite o valor de 'c': "))
+
+# Calcular as raizes
+resultado = calcular_raizes(a, b, c)
+
+# Mostra o resultado
+if isinstance(resultado, str):
+    print(resultado)
+else:
+    raiz1, raiz2 = resultado
+    print(f"As raízes da equação são: {raiz1:.5f} e {raiz2:.5f}")
+
+
